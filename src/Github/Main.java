6f9c6de3e6a09dbb94c16390b7ea09a0458bbb03
browner86 <browner86@gmail.com>
2020-10-08 18:42:27 -0400

    added watermelons

--- src/Github/Main.java
+++ src/Github/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Brian.");
         System.out.println("Is there anyone out there that can hear me?");
+        System.out.println("Watermelons");
     }
 }
